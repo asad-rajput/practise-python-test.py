52e2239213c81b58e796fc3fb97169469e842e04
asad-rajput <117216624+asad-rajput@users.noreply.github.com>
2022-11-02 00:14:18 +0500

    Add files via upload

---
+++ don.py
@@ -0,0 +1,85 @@
+"""
+a=str(3)
+x=float(3)
+print(a)
+print(x)
+a=5
+b="hello"
+print(type(a))
+print(type(b))
+x="asad"
+b='ali'
+
+print(x)
+print(b)
+a='han'
+A='na'
+print(a)
+print(A)
+myname="gg"
+my_name="gg"
+_myname="gg"
+MYNAME="gg"
+myname2="gg"
+print(myname)
+print(my_name)
+print(_myname)
+print(MYNAME)
+print(myname2)
+a,b,c='asad','ali','athar'
+print(a,b,c)
+a=b=c='asad'
+print(a,b,c)
+names = ['asad','ali','athar']
+a=b=c=names
+print(a)
+print(b)
+print(c)
+
+GLOBAL VARIABLES....
+a="asad ali"
+def myfunc():
+ print("myname is",a)
+myfunc()
+
+a="asad"
+def myfunc():
+ a="athar"
+print("yes i am",a)
+myfunc()
+print("no i am",a) 
+
+def myfunc():
+    global x
+    x="asad"
+myfunc()
+print("yeah i am ",x)
+
+x={'name':'john','age':36}
+print(type(x))
+
+x = 1
+y = 35656222554887711
+z = -3255522
+  
+print(type(x))
+print(type(y))
+print(type(z))
+
+x=1
+y=1.4
+z=2j
+a=float(x)
+b=int(y)
+c=complex(x)
+print(a)
+print(b)
+print(c)
+print(type(a))
+print(type(b))
+print(type(c))
+
+#random numbers....
+import random
+print(random.randrange(1,10))
+"""
