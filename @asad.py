37db7853da3c4afbadd2234a3b9800ea07ddeb32
asad-rajput <117216624+asad-rajput@users.noreply.github.com>
2022-11-18 00:21:28 +0500

    Add files via upload

--- @asad.py
+++ @asad.py
@@ -1,4 +1,25 @@
+'''
 a=int(input("enter the value"))
 print("the num is : ", a)
 for i in range(1,11): 
     print(a,"*",i,"=",a*i)
+    '''
+distance=[1,2,5,6,3,7,4,9]
+print(len(distance))
+
+if len(distance) > 2:
+    distance.pop(0)
+    distance.append(5)
+    print("the new list is:",distance)
+else:
+     distance.append(5)
+
+
+for i in range(len(distance)):
+    print(distance[i])
+    if distance[i] == 5:
+        break
+    else:    
+        print(distance[i])
+         
+
